98fef5589bf65b27224a816ad373bc729a1663fc
Pankaj Kandpal <pankajkandpal99@gmail.com>
2023-10-03 21:49:44 +0530

    changed commit

--- sum.java
+++
@@ -1,8 +0,0 @@
-public class sum{
-public static void main (String []args){
-    int a= 150;
-    int b= 250;
-    int sum = a+b;
-    System.out.println("The sum of two variables is +"+sum);
-}
-}
--- sum.js
+++ sum.js
@@ -1,1 +1,2 @@
 console.log('Hi Sir!');
+console.log('I want become a full stack developer.');
